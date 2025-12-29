a7719f5d6c3ea379491c98b2d41b5a9cef5e1769
Ashish Sarule <saruleashish2003@gmail.com>
2025-12-29 11:03:52 +0530

    new commit

--- constra/urls.py
+++ constra/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('constra/',include("core.urls")),
+    path('constra/',include("constraApp.urls")),
 ]
